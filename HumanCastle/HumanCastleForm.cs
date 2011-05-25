36efde51c50a8d4e2667aa3c1556d1fb0687e72c
Michael B. E. Rickert <nobody@nowhere.noplace>
2011-05-24 21:48:54 -0700

    Basic render loop

--- HumanCastle/HumanCastleForm.cs
+++ HumanCastle/HumanCastleForm.cs
@@ -1,23 +1,48 @@
 ﻿using System;
 using System.Drawing;
 using System.Windows.Forms;
+using SlimDX.Direct3D9;
 using SlimDX.Windows;
 
 namespace HumanCastle {
 	[System.ComponentModel.DesignerCategory("")]
 	class HumanCastleForm : Form {
+		Direct3D D3D;
+		Device   Device;
+
 		public HumanCastleForm() {
 			ClientSize = new Size(800,600);
 			Text = "Human Castle";
+
+			D3D = new Direct3D();
+			SetupDevice();
 		}
 
 		protected override void Dispose( bool disposing ) {
 			if ( disposing ) {
+				TeardownDevice();
+				using ( D3D ) {}
 			}
 			base.Dispose(disposing);
 		}
 
+		void SetupDevice() {
+			Device = new Device(D3D,0,DeviceType.Hardware,Handle,CreateFlags.HardwareVertexProcessing,new PresentParameters()
+				{ BackBufferCount    = 1
+				, BackBufferFormat   = Format.X8R8G8B8
+				, BackBufferWidth    = ClientSize.Width
+				, BackBufferHeight   = ClientSize.Height
+				, DeviceWindowHandle = Handle
+				, Windowed           = true
+				});
+		}
+		void TeardownDevice() {
+			using ( Device ) {}
+		}
+
 		void MainLoop() {
+			Device.Clear( ClearFlags.Target, unchecked((int)0xFF112233u), 0f, 0 );
+			Device.Present();
 		}
 
 		[STAThread] static void Main() {
