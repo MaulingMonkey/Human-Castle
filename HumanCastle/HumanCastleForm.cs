662f1c6e3ee7f9897d9ba2d7af6c2f777059bbd9
Michael B. E. Rickert <nobody@nowhere.noplace>
2011-05-24 21:29:47 -0700

    Empty project skeleton

---
+++ HumanCastle/HumanCastleForm.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+using SlimDX.Windows;
+
+namespace HumanCastle {
+	[System.ComponentModel.DesignerCategory("")]
+	class HumanCastleForm : Form {
+		public HumanCastleForm() {
+			ClientSize = new Size(800,600);
+			Text = "Human Castle";
+		}
+
+		protected override void Dispose( bool disposing ) {
+			if ( disposing ) {
+			}
+			base.Dispose(disposing);
+		}
+
+		void MainLoop() {
+		}
+
+		[STAThread] static void Main() {
+			Application.EnableVisualStyles();
+			Application.SetCompatibleTextRenderingDefault(false);
+			var form = new HumanCastleForm();
+			MessagePump.Run( form, form.MainLoop );
+		}
+	}
+}
