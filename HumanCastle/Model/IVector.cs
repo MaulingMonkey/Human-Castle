db494b1ccb6ab1fa6db25c4f565aa77c135004ad
Albert <albertywang@gmail.com>
2011-05-25 01:05:56 -0500

    Adding Ivector.cs and A* pathfinding

---
+++ HumanCastle/Model/IVector.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace HumanCastle.Model
+{
+	struct IVector3
+	{
+		public static IVector3 Zero = new IVector3(0, 0, 0);
+		
+		public static int Dot(IVector3 a, IVector3 b)
+		{
+			return a.X * b.X + a.Y * b.Y + a.Z * b.Z;
+		}
+
+		public static double Distance(IVector3 a, IVector3 b)
+		{
+			double d = Dot(a, b);
+			return Math.Sqrt(d);
+		}
+
+		public int X, Y, Z;
+		public IVector3(int x, int y, int z)
+		{
+			X = x;
+			Y = y;
+			Z = z;
+		}
+
+		//Some basic operations.
+		public bool BoundedBy(IVector3 min, IVector3 max)
+		{
+			if (X >= min.X && Y >= min.Y && Z >= min.Z &&
+				X < max.X && Y < max.Y && Z < max.Z)
+			{
+				return true;
+			}
+			return false;
+		}
+
+		public static bool operator ==(IVector3 a, IVector3 b)
+		{
+			return a.X == b.X && a.Y == b.Y && a.Z == b.Z;
+		}
+
+		public static bool operator !=(IVector3 a, IVector3 b)
+		{
+			return a.X != b.X || a.Y != b.Y || a.Z != b.Z;
+		}
+
+		public int GetHashCode()
+		{
+			return X ^ (Y * 263) + Z * 773;
+		}
+	}
+}
--- HumanCastle/Model/LocalMap.cs
+++ HumanCastle/Model/LocalMap.cs
@@ -80,28 +80,73 @@ namespace HumanCastle.Model
 		}
 
 		//Distance estimator <_<
-		private int Cost(IVector3 node, IVector3 end)
+		private double Cost(IVector3 node, IVector3 end)
 		{
 			return IVector3.Dot(node, end);
 		}
 
+		private TileDeclaration TileAt(IVector3 target)
+		{
+			Debug.Assert(target.BoundedBy(IVector3.Zero, Dimensions));
+			return tiles[target.X, target.Y, target.Z].decl;
+		}
+
+		private List<IVector3> passableNodes(IVector3 node)
+		{
+			var result = new List<IVector3>();
+
+			for (int x = -1; x <= 1; ++x)
+			{
+				for (int y = -1; y <= 1; ++y)
+				{
+					if (y == 0 && x == 0)
+					{
+						continue;
+					}
+
+					IVector3 target = node;
+					target.X += x;
+					target.Y += y;
+
+					if (target.BoundedBy(IVector3.Zero, Dimensions) && 
+						TileAt(target).passable)
+					{
+						result.Add(target);
+					}
+				}
+			}
+			return result;
+		}
+
+		public void reconstruct(Dictionary<IVector3, IVector3> camefrom, IVector3 current, List<IVector3> result)
+		{
+			if (camefrom.ContainsKey(current))
+			{
+				reconstruct(camefrom, camefrom[current], result);
+			}
+			result.Add(current);
+		}
+
 		public List<IVector3> Path(IVector3 start, IVector3 end)
 		{
-			Debug.Assert(Dimensions.BoundedBy(start, end));
+			Debug.Assert(start.BoundedBy(IVector3.Zero, Dimensions));
+			Debug.Assert(end.BoundedBy(IVector3.Zero, Dimensions));
 
 			List<IVector3> result = new List<IVector3>();
-			
-			List<IVector3> closed = new List<IVector3>();
-			List<IVector3> open = new List<IVector3>();
+
+			HashSet<IVector3> closed = new HashSet<IVector3>();
+			HashSet<IVector3> open = new HashSet<IVector3>();
 			open.Add(start);
 
-			var goalScore = new Dictionary<IVector3, int>();
+			var cameFrom = new Dictionary<IVector3, IVector3>();
+
+			var goalScore = new Dictionary<IVector3, double>();
 			goalScore.Add(start, 0);
 
-			var guessScore = new Dictionary<IVector3, int>();
+			var guessScore = new Dictionary<IVector3, double>();
 			guessScore.Add(start, Cost(start, end));
 
-			var finalScore = new Dictionary<IVector3, int>();
+			var finalScore = new Dictionary<IVector3, double>();
 			finalScore.Add(start, guessScore[start]);
 
 			while(open.Count != 0)
@@ -112,14 +157,49 @@ namespace HumanCastle.Model
 				if (node == end)
 				{
 					//Uhhh.
+					reconstruct(cameFrom, cameFrom[end], result);
 					return result;
 				}
 
 				open.Remove(node);
 				closed.Add(node);
 
-				
+				foreach (var t in passableNodes(node))
+				{
+					if (closed.Contains(t))
+					{
+						continue;
+					}
+
+					bool isBetter = false;
+					double score = goalScore[node] + IVector3.Distance(node, t);
+					if (!open.Contains(t))
+					{
+						open.Add(t);
+						isBetter = true;
+					}
+					else if (score < goalScore[t])
+					{
+						isBetter = true;
+					}
+					else
+					{
+						isBetter = false;
+					}
+
+					if (isBetter)
+					{
+						//Ummm.
+						cameFrom[t] = node;
+						goalScore[t] = score;
+						guessScore[t] = Cost(t, end);
+						finalScore[t] = score + Cost(t, end);
+					}
+				}
 			}
+
+			//No path! 
+			return null;
 		}
 	}
 }
